9748dc9cad59b336bca120f5decd040f84c03d23
earlbellinger <earlbellinger@gmail.com>
2025-03-01 23:16:49 -0500

    populate data module

--- seistron/__init__.py
+++ seistron/__init__.py
@@ -5,3 +5,11 @@ __version__ = version("seistron")
 
 __author__ = """Earl Patrick Bellinger"""
 __email__ = 'earl.bellinger@yale.edu'
+
+from .data import *
+from .emulate import *
+from .hpc import *
+from .models import *
+from .sample import *
+from .visualize import *
+from .utils import *
